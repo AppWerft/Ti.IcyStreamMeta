5dbf937489fad1d1a4a2ebd0588bf387fbe8efd1
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-07-08 17:22:00 +0200

          

---
+++ android/build/generated/obj/local/armeabi-v7a/objs/de.appwerft.icystreammeta/de.appwerft.icystreammeta.IcyMetaModule.o.d
@@ -0,0 +1,68 @@
+/var/folders/25/yvdn9h053sv3htdkzq8558k40000gn/T//fuerst/icystreammeta-generated/obj/local/armeabi-v7a/objs/de.appwerft.icystreammeta/de.appwerft.icystreammeta.IcyMetaModule.o: \
+ jni/de.appwerft.icystreammeta.IcyMetaModule.cpp \
+ jni/de.appwerft.icystreammeta.IcyMetaModule.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/Proxy.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/v8.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/v8stdint.h \
+ /Users/fuerst/Library/android-ndk-macosx/sources/cxx-stl/stlport/stlport/stddef.h \
+ /Users/fuerst/Library/android-ndk-macosx/sources/cxx-stl/stlport/stlport/stdio.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/JavaObject.h \
+ /Users/fuerst/Library/android-ndk-macosx/sources/cxx-stl/stlport/stlport/assert.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/EventEmitter.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/NativeObject.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/AndroidUtil.h \
+ /Users/fuerst/Library/android-ndk-macosx/sources/cxx-stl/stlport/stlport/stdlib.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/EventEmitter.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/JNIUtil.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/AndroidUtil.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/JSException.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/ProxyFactory.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/TypeConverter.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/V8Util.h \
+ jni/de.appwerft.icystreammeta.IcyMetaProxy.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/org.appcelerator.kroll.KrollModule.h \
+ /Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/Proxy.h
+
+jni/de.appwerft.icystreammeta.IcyMetaModule.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/Proxy.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/v8.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/v8stdint.h:
+
+/Users/fuerst/Library/android-ndk-macosx/sources/cxx-stl/stlport/stlport/stddef.h:
+
+/Users/fuerst/Library/android-ndk-macosx/sources/cxx-stl/stlport/stlport/stdio.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/JavaObject.h:
+
+/Users/fuerst/Library/android-ndk-macosx/sources/cxx-stl/stlport/stlport/assert.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/EventEmitter.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/NativeObject.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/AndroidUtil.h:
+
+/Users/fuerst/Library/android-ndk-macosx/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/EventEmitter.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/JNIUtil.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/AndroidUtil.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/JSException.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/ProxyFactory.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/TypeConverter.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/V8Util.h:
+
+jni/de.appwerft.icystreammeta.IcyMetaProxy.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/org.appcelerator.kroll.KrollModule.h:
+
+/Library/Application\ Support/Titanium/mobilesdk/osx/5.2.0.GENPERM/android/native/include/Proxy.h:
